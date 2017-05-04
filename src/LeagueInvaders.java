4e59bfe549953f4b40d633d776606b303168fdb5
tactreconx <league@Admins-Mac-mini.local>
2017-05-04 16:59:07 -0700

    Started League Invaders

---
+++ src/LeagueInvaders.java
@@ -0,0 +1,24 @@
+import javax.swing.JFrame;
+
+public class LeagueInvaders {
+
+	GamePanel panel = new GamePanel();
+	
+	LeagueInvaders(){
+		setup();
+	}
+	
+	public static void main(String[] args){
+		new LeagueInvaders();
+		
+	}
+	
+	void setup(){
+		JFrame frame = new JFrame();
+		frame.add(panel);
+		frame.setSize(500, 800);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel.StartGame();
+	}
+}
