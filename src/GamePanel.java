702a1f883ce4360f08c9412ca6a5ecac14d76241
WEMF00 <league@Mac-mini-4.local>
2017-05-25 16:57:37 -0700

    Finished LeagueInvaders pg 6
    We in this

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,14 +8,27 @@ import java.awt.event.KeyListener;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
+
+
+
 public class GamePanel extends JPanel implements ActionListener, KeyListener{
+	final int MENU_STATE = 0;
+	final int GAME_STATE = 1;
+	final int END_STATE = 2;
+	int currentState = MENU_STATE;
 		Timer time = new Timer( 1000/60, this);
-		GameObject object = new GameObject();
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			repaint();
-			object.update();
+			if(currentState == MENU_STATE){
+				updateMenuState();
+			}else if(currentState == GAME_STATE){
+				updateGameState();
+			}else if(currentState == END_STATE){
+				updateEndState();
+			}
+
 			
 		}
 		
@@ -23,24 +37,65 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 		}
 		
 		public void paintComponent(Graphics g){
-			object.draw(g);
+			if(currentState == MENU_STATE){
+				drawMenuState(g);
+			}else if(currentState == GAME_STATE){
+				drawGameState(g);
+			}else if(currentState == END_STATE){
+				drawEndState(g);
+			}
+
 		}
 
 		@Override
 		public void keyTyped(KeyEvent e) {
 			// TODO Auto-generated method stub
-			System.out.println("A message to the console");
+			//System.out.println("A message to the console");
 		}
 
 		@Override
 		public void keyPressed(KeyEvent e) {
 			// TODO Auto-generated method stub
-			System.out.println("A message to the console");
+			
+			if (e.getKeyCode() == KeyEvent.VK_ENTER){
+				currentState += 1; 
+				System.out.println("A message to the console");
+				if(currentState > END_STATE){
+					currentState = MENU_STATE;
+				}
+
+			}
 		}
 
 		@Override
 		public void keyReleased(KeyEvent e) {
 			// TODO Auto-generated method stub
-			System.out.println("A message to the console");
+			//System.out.println("A message to the console");
+		}
+		
+		public void updateMenuState(){
+			
+		}
+		public void updateGameState(){
+			
+		}
+		
+		public void updateEndState(){
+			
+		}
+		
+		public void drawMenuState(Graphics g){
+			g.setColor(Color.BLUE);
+			g.fillRect(0, 0, 500, 800);
+
+		}
+		public void drawGameState(Graphics g){
+			g.setColor(Color.BLACK);
+			g.fillRect(0, 0, 500, 800);
+		}
+		
+		public void drawEndState(Graphics g){
+			g.setColor(Color.RED);
+			g.fillRect(0, 0, 500, 800);
 		}
 }
